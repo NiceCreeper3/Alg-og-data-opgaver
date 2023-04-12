d29d32ff960ce079b6660ed6c1f5f598738f2f0e
NiceCreeper3 <124564568+NiceCreeper3@users.noreply.github.com>
2023-04-12 10:51:13 +0200

    OpVer0

---
+++ Opgave4.3.3/Opgave4.3.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opgave4._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
